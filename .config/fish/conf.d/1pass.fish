2c295adf5430e5c5eaa0c3a9ea4c427339c5e7de
Aleksandr Dobdin <adobdin@users.noreply.github.com>
2026-04-17 12:04:19 +0200

    Update 1pass.fish
    load op every time to get k8s config

--- .config/fish/conf.d/1pass.fish
+++ .config/fish/conf.d/1pass.fish
@@ -1,131 +1,189 @@
-############################################################
-# kube runtime
-############################################################
-
-function k
-    set tmp (mktemp)
-
-    printf "%s\n" "$KUBE_DATA_CACHE" > $tmp
-
-    env KUBECONFIG=$tmp kubectl $argv
-    set rc $status
-
-    rm -f $tmp
-    return $rc
-end
-
-function kctx
-    set tmp (mktemp)
-
-    printf "%s\n" "$KUBE_DATA_CACHE" > $tmp
-
-    env KUBECONFIG=$tmp kubectx $argv
-    set rc $status
-
-    rm -f $tmp
-    return $rc
-end
-
-function kns
-    set tmp (mktemp)
-
-    printf "%s\n" "$KUBE_DATA_CACHE" > $tmp
-
-    env KUBECONFIG=$tmp kubens $argv
-    set rc $status
-
-    rm -f $tmp
-    return $rc
-end
-
-function k-logout
-    op signout
-    set -e KUBE_DATA_CACHE
-    rm -f $KUBE_RUNTIME_CONFIG
-    echo "Logged out and cache cleared."
-end
-
-function k-select
-
-    set item_id (
-        op item list --tags k8s-config --format json |
-        jq -r '.[] | [.id, .title, .vault.name, (.tags|join(","))] | @tsv' |
-        sort -k2 |
-        fzf --delimiter='\t' --with-nth=2,3,4 --tabstop=30 |
-        cut -f1
-    )
-
-    if test -z "$item_id"
-        return
+function _k_require_selection
+    if test -z "$KUBE_ITEM_ID" -o -z "$KUBE_VAULT_NAME"
+        echo "Error: No kubeconfig selected. Run 'k-select' first." >&2
+        return 1
     end
+end
 
-    set item_title (op item get "$item_id" --format json | jq -r '.title')
-    set vault_name (op item get "$item_id" --format json | jq -r '.vault.name')
+function _k_fetch_raw_config
+    _k_require_selection
+    or return 1
 
-    printf "Loading config for: %s\n" "$item_title"
+    set -l raw_data ""
+    set -l file_id ""
 
-    set raw_data (op read "op://$vault_name/$item_id/k8s config" 2>/dev/null | string collect)
+    set raw_data (op read "op://$KUBE_VAULT_NAME/$KUBE_ITEM_ID/k8s config" 2>/dev/null | string collect)
 
     if test -z "$raw_data"
-        set raw_data (op read "op://$vault_name/$item_id/text" 2>/dev/null | string collect)
+        set raw_data (op read "op://$KUBE_VAULT_NAME/$KUBE_ITEM_ID/text" 2>/dev/null | string collect)
     end
 
     if test -z "$raw_data"
-        set file_id (op item get "$item_id" --format json | grep -oE '"files" ?: ?\[\{"id" ?: ?"[^"]+"' | cut -d'"' -f8)
-
+        set file_id (op item get "$KUBE_ITEM_ID" --format json | jq -r '.files[0].id // empty')
         if test -n "$file_id"
-            set raw_data (op read "op://$vault_name/$item_id/$file_id" 2>/dev/null | string collect)
+            set raw_data (op read "op://$KUBE_VAULT_NAME/$KUBE_ITEM_ID/$file_id" 2>/dev/null | string collect)
         end
     end
 
     if test -z "$raw_data"
-        printf "Error: Data not found in fields (k8s config, text) or files.\n"
-        return
-    end
-
-    set -gx KUBE_DATA_CACHE "$raw_data"
-
-    printf "Success! Context set for: %s\n" "$item_title"
-
-end
-
-function ctx
-
-    if test -z "$KUBE_DATA_CACHE"
-        echo "Error: No kubeconfig loaded. Run 'k-select' first."
+        echo "Error: Data not found in fields (k8s config, text) or files." >&2
         return 1
     end
 
-    set tmp (mktemp)
+    printf "%s" "$raw_data"
+end
 
-    printf "%s\n" "$KUBE_DATA_CACHE" > $tmp
+function _k_with_temp_config
+    _k_require_selection
+    or return 1
 
-    set current (kubectl --kubeconfig $tmp config current-context)
+    set -l tmp (mktemp)
+    or return 1
 
-    set contexts (kubectl --kubeconfig $tmp config get-contexts -o name)
+    chmod 600 $tmp 2>/dev/null
 
-    set ordered (printf "%s\n%s\n" $current $contexts | awk '!seen[$0]++')
+    _k_fetch_raw_config > $tmp
+    set -l rc $status
 
-    set target (printf "%s\n" $ordered | fzf \
-        --reverse \
-        --header "Select Cluster Context (current: $current)" \
-        --height 80% \
-        --preview "kubectl --kubeconfig $tmp config view --minify --context {}" \
-        --preview-window right:50%:wrap)
-
-    if test -n "$target"
-        kubectl --kubeconfig $tmp config use-context $target >/dev/null 2>&1
-        set -gx KUBE_DATA_CACHE (kubectl --kubeconfig $tmp config view --raw | string collect)
-        echo "Switched to context: $target"
+    if test $rc -ne 0
+        rm -f $tmp
+        return $rc
     end
 
-    rm -f $tmp
+    env KUBECONFIG=$tmp $argv
+    set rc $status
 
+    rm -f $tmp
+    return $rc
+end
+
+function k-select
+    set -l selection (
+        op item list --tags k8s-config --format json |
+        jq -r '.[] | [.id, .title, .vault.name, (.tags|join(","))] | @tsv' |
+        sort -k2 |
+        fzf --delimiter='\t' --with-nth=2,3,4 --tabstop=30 --reverse --inline-info
+    )
+
+    if test -z "$selection"
+        return 1
+    end
+
+    set -l fields (string split \t -- $selection)
+
+    set -gx KUBE_ITEM_ID $fields[1]
+    set -gx KUBE_ITEM_TITLE $fields[2]
+    set -gx KUBE_VAULT_NAME $fields[3]
+
+    set -e KUBE_CONTEXT_NAME
+    set -e KUBE_NAMESPACE
+
+    echo "Selected kubeconfig: $KUBE_ITEM_TITLE"
+end
+
+function k
+    _k_require_selection
+    or return 1
+
+    set -l extra_args
+
+    if test -n "$KUBE_CONTEXT_NAME"
+        set extra_args $extra_args --context "$KUBE_CONTEXT_NAME"
+    end
+
+    if test -n "$KUBE_NAMESPACE"
+        set -l has_ns 0
+        for arg in $argv
+            if test "$arg" = "-n" -o "$arg" = "--namespace" -o "$arg" = "-A" -o "$arg" = "--all-namespaces"
+                set has_ns 1
+                break
+            end
+        end
+
+        if test $has_ns -eq 0
+            set extra_args $extra_args -n "$KUBE_NAMESPACE"
+        end
+    end
+
+    _k_with_temp_config kubectl $extra_args $argv
+end
+
+function ctx
+    _k_require_selection
+    or return 1
+
+    set -l current (_k_with_temp_config kubectl config current-context 2>/dev/null)
+    set -l contexts (_k_with_temp_config kubectl config get-contexts -o name)
+
+    set -l ordered (
+        printf "%s\n%s\n" "$current" $contexts | awk 'NF && !seen[$0]++'
+    )
+
+    set -l target (
+        printf "%s\n" $ordered | fzf \
+            --reverse \
+            --header "Select Cluster Context (current: $current)" \
+            --height 80%
+    )
+
+    if test -n "$target"
+        set -gx KUBE_CONTEXT_NAME "$target"
+        echo "Context selected: $target"
+    end
+end
+
+function kctx
+    if test (count $argv) -eq 0
+        ctx
+        return $status
+    end
+
+    set -gx KUBE_CONTEXT_NAME "$argv[1]"
+    echo "Context selected: $KUBE_CONTEXT_NAME"
+end
+
+function kns
+    _k_require_selection
+    or return 1
+
+    if test (count $argv) -gt 0
+        set -gx KUBE_NAMESPACE "$argv[1]"
+        echo "Namespace selected: $KUBE_NAMESPACE"
+        return 0
+    end
+
+    set -l current_ns default
+    if test -n "$KUBE_NAMESPACE"
+        set current_ns "$KUBE_NAMESPACE"
+    end
+
+    set -l target_ns (
+        k get ns -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' 2>/dev/null |
+        fzf \
+            --reverse \
+            --header "Select Namespace (current: $current_ns)" \
+            --height 80%
+    )
+
+    if test -n "$target_ns"
+        set -gx KUBE_NAMESPACE "$target_ns"
+        echo "Namespace selected: $target_ns"
+    end
+end
+
+function k-logout
+    set -e KUBE_ITEM_ID
+    set -e KUBE_ITEM_TITLE
+    set -e KUBE_VAULT_NAME
+    set -e KUBE_CONTEXT_NAME
+    set -e KUBE_NAMESPACE
+
+    op signout >/dev/null 2>/dev/null
+    echo "Logged out and selection cleared."
 end
 
 function ssh-select
-
-    set selection (
+    set -l selection (
         op item list --tags ssh-key --format json |
         jq -r '.[] | [.id, .title, .vault.id, .vault.name, (.tags|join(","))] | @tsv' |
         sort -k2 |
@@ -133,18 +191,18 @@ function ssh-select
     )
 
     if test -z "$selection"
-        return
+        return 1
     end
 
-    set fields (string split \t -- $selection)
-
-    set item_id $fields[1]
-    set item_title $fields[2]
-    set vault_id $fields[3]
+    set -l fields (string split \t -- $selection)
+    set -l item_id $fields[1]
+    set -l item_title $fields[2]
+    set -l vault_id $fields[3]
 
     echo "Adding key: $item_title"
 
-    set tmp_key "/tmp/op_ssh_key_$fish_pid"
+    set -l tmp_key (mktemp)
+    or return 1
 
     op item get "$item_id" --vault "$vault_id" --reveal --format json | \
         jq -r '
@@ -164,8 +222,8 @@ function ssh-select
     end
 
     chmod 600 $tmp_key
-
     ssh-add -t 1h $tmp_key
-
+    set -l rc $status
     rm -f $tmp_key
+    return $rc
 end
